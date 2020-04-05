9ac80d684d65987c9f5df7d1233c2154fb6d796b
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-04-05 15:20:34 -0600

    Basic Express and Templating

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+//sets the view engine as pug for render requests
+app.set('view engine', 'pug');
+//serve static files (CSS)
+app.use('/static', express.static('public/stylesheets'));
+
+//home page render
+app.get('/', (req, res) =>{
+    res.render('main')
+})
+
+//creates error
+app.use((req, res, next)=>{
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+})
+
+//error handler
+app.use(( err, req, res, next ) => {
+    res.locals.error = err;
+    if (err.status >= 100 && err.status < 600)
+      res.status(err.status);
+    else
+      res.status(500);
+    res.render('error');
+  });
+  
+
+//starts listening on specfied port(localhost:3000). Server must be started with node app.js in terminal
+app.listen(3000, () =>{
+    console.log('The server is up and running')
+});
