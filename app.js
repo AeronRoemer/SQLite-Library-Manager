c4c6b74b73665589f2041a7b91331048793d69f0
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-04-06 22:00:12 -0600

    Pug/Routes Basic setup w db

--- app.js
+++ app.js
@@ -1,36 +1,68 @@
 const express = require('express');
 var sequelize = require("./models").sequelize;
+const Book = require('./models').Book;
+const routes = require('./routes');
+
+//set up all views in pug
+//set up new book edit book
+//validation
+//404 route
+//create search function
+//integrate serach function
+//css improvements
+//a11y check
 
 const app = express();
 //sets the view engine as pug for render requests
 app.set('view engine', 'pug');
 //serve static files (CSS)
 app.use('/static', express.static('public/stylesheets'));
+//links route for main
+app.use(routes)
 
-
-//home page render
-app.get('/', (req, res) =>{
-    res.render('books')
-})
 
 //creates error
 app.use((req, res, next)=>{
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 })
 
 //error handler
 app.use(( err, req, res, next ) => {
-    res.locals.error = err;
-    if (err.status >= 100 && err.status < 600)
-      res.status(err.status);
-    else
-      res.status(500);
-    res.render('error');
-  });
-  
+  res.locals.error = err;
+  if (err.status >= 100 && err.status < 600)
+    res.status(err.status);
+  else
+    res.status(500);
+  res.render('404NoFound');
+});
 
+  
+//   (async () => {
+//     //Movie.sync() synchronizes table from db, sequelize.sync() does all
+//     await sequelize.sync( 
+//         //force completely drops & recreates table each time
+//         { force: true } );
+//   try{
+//     // Instance of the Movie class represents a database row
+//     const book = await Book.create({
+//         title: "A Brief History of Time",
+//         author: "Stephen Hawking",
+//         genre: "Non Fiction",
+//         year: 1988
+//     })
+//     await Book.create({
+//         title: "Armada",
+//         author: "Ernest Cline",
+//         genre: "Science Fiction",
+//         year: 2015
+//     })
+//   } catch(error){
+//     console.error("Database error: ", error)
+//   }
+
+// })();
 //starts listening on specfied port(localhost:3000). Server must be started with node app.js in terminal
 sequelize.sync().then(app.listen(3000, () =>{
     console.log('The server is up and running')
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const router = express.Router();
+const Books = require('../models').Book
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+    return async(req, res, next) => {
+      try {
+        await cb(req, res, next)
+      } catch(error){
+        res.status(500).send(error);
+      }
+    }
+  }
+
+//home page render
+router.get('/', asyncHandler(async (req, res) =>{
+const books = await Books.findAll();
+    res.render('books', { books })
+}))
+
+
+module.exports = router
